1180e0c49b0695b628934b1201684a3196f934c5
qMedop <95769954+qMedop@users.noreply.github.com>
2023-05-18 17:22:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,3 +51,64 @@ previous.addEventListener('click' , () => {
     next.style.display = 'none'
   }
 })
+
+let accPiccontainer = document.querySelector("nav .account")
+let accPic = document.querySelector("nav .login img")
+let accContainer = document.querySelector("nav .account ")
+let accSettings = document.querySelector("nav .account .account-setting")
+let closeSettings = document.querySelector(".account .account-setting .account-s .close")
+let accContainerValue = 0
+
+accPic.addEventListener('mouseenter', () => {
+  if(window.innerWidth > 520) {
+      accPic.style.border = '1px solid #3ea6ff'
+  }
+})
+accPic.addEventListener('mouseleave', () => {
+  if(window.innerWidth > 520) {
+    accPic.style.border = '0px solid #3ea6ff'
+}})
+
+  accPic.addEventListener('click', () => {
+    if(window.innerWidth > 520) {
+    accContainer.style.display = 'block'
+    accPic.style.border = '1px solid #3ea6ff'
+    accSettings.style.top = '10px'
+    setTimeout(() => {
+      accContainer.style.opacity = '1'
+      accSettings.style.right = '80px'
+    }, 100);
+  }
+  })
+  accContainer.addEventListener('click', () => {
+    if(window.innerWidth > 520) {
+    accContainer.style.opacity = '0'
+    accPic.style.border = '0px solid #3ea6ff'
+    accSettings.style.right = '0px'
+    setTimeout(() => {
+      accContainer.style.display = 'none'
+    }, 300);
+  }
+  })
+
+  accPic.addEventListener('click', () => {
+    if(window.innerWidth <= 520) {
+      accSettings.style.top = '100%'
+    accContainer.style.display = 'block'
+    accPic.style.border = '1px solid #3ea6ff'
+    setTimeout(() => {
+      accContainer.style.opacity = '1'
+      accSettings.style.top = '0'
+    }, 100);
+  }
+  })
+  closeSettings.addEventListener('click', () => {
+    if(window.innerWidth <= 520) {
+    accContainer.style.opacity = '0'
+    accPic.style.border = '0px solid #3ea6ff'
+    accSettings.style.top = '100%'
+    setTimeout(() => {
+      accContainer.style.display = 'none'
+    }, 300);
+  }
+  })
