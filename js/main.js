429b52af1f5836f333b1950c85a1bf4b3d49f232
qMedop <95769954+qMedop@users.noreply.github.com>
2023-05-19 16:30:54 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 let searchInput = document.getElementById("input-search")
 let searchdiv = document.querySelector(".search")
+let NotfiacationActive = '<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block;"><g class="style-scope yt-icon"><path d="M10,20h4c0,1.1-0.9,2-2,2S10,21.1,10,20z M20,17.35V19H4v-1.65l2-1.88v-5.15c0-2.92,1.56-5.22,4-5.98V3.96	c0-1.42,1.49-2.5,2.99-1.76C13.64,2.52,14,3.23,14,3.96l0,0.39c2.44,0.75,4,3.06,4,5.98v5.15L20,17.35z" class="style-scope yt-icon"></path></g></svg>'
+let Notfiacationidle = '<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="pointer-events: none; display: block;"><g class="style-scope yt-icon"><path d="M10,20h4c0,1.1-0.9,2-2,2S10,21.1,10,20z M20,17.35V19H4v-1.65l2-1.88v-5.15c0-2.92,1.56-5.22,4-5.98V3.96 c0-1.42,1.49-2.5,2.99-1.76C13.64,2.52,14,3.23,14,3.96l0,0.39c2.44,0.75,4,3.06,4,5.98v5.15L20,17.35z M19,17.77l-2-1.88v-5.47 c0-2.47-1.19-4.36-3.13-5.1c-1.26-0.53-2.64-0.5-3.84,0.03C8.15,6.11,7,7.99,7,10.42v5.47l-2,1.88V18h14V17.77z" class="style-scope yt-icon"></path></g></svg>'
+let topCheck = 0
 
-searchInput.onfocus = function() {
-  searchdiv.style.border = "1px solid #1c62b9"
-  console.log("g");
+function refresh() {
+  if (window.innerWidth == 520 || window.innerWidth == 680) {
+    location.reload();
+  }
 }
-searchInput.addEventListener ('focusout', () => {
+
+window.onresize = refresh
+
+searchInput.onfocus = function () {
+  searchdiv.style.border = "1px solid #1c62b9"
+}
+searchInput.addEventListener('focusout', () => {
   searchdiv.style.border = "1px solid #272727"
-  console.log('s');
 })
 
 let sugg = document.querySelector(".sugg")
@@ -16,7 +25,7 @@ let next = document.querySelector(".next")
 let test = 0
 
 
-next.addEventListener('click' , () => {
+next.addEventListener('click', () => {
   sugg.scrollBy(
     {
       left: 2000,
@@ -25,12 +34,12 @@ next.addEventListener('click' , () => {
   )
   previous.style.display = 'flex'
   next.style.display = 'none'
-  if(window.innerWidth <= 520) {
+  if (window.innerWidth <= 520) {
     previous.style.display = 'none'
     next.style.display = 'none'
   }
 })
-previous.addEventListener('click' , () => {
+previous.addEventListener('click', () => {
   sugg.scrollBy(
     {
       left: -2000,
@@ -39,7 +48,7 @@ previous.addEventListener('click' , () => {
   )
   previous.style.display = 'none'
   next.style.display = 'flex'
-  if(window.innerWidth <= 520) {
+  if (window.innerWidth <= 520) {
     previous.style.display = 'none'
     next.style.display = 'none'
   }
@@ -53,17 +62,18 @@ let closeSettings = document.querySelector(".account .account-setting .account-s
 let accContainerValue = 0
 
 accPic.addEventListener('mouseenter', () => {
-  if(window.innerWidth > 520) {
-      accPic.style.border = '1px solid #3ea6ff'
+  if (window.innerWidth > 520) {
+    accPic.style.border = '1px solid #3ea6ff'
   }
 })
 accPic.addEventListener('mouseleave', () => {
-  if(window.innerWidth > 520) {
+  if (window.innerWidth > 520) {
     accPic.style.border = '0px solid #3ea6ff'
-}})
+  }
+})
 
-  accPic.addEventListener('click', () => {
-    if(window.innerWidth > 520) {
+accPic.addEventListener('click', () => {
+  if (window.innerWidth > 520) {
     accContainer.style.display = 'block'
     accPic.style.border = '1px solid #3ea6ff'
     accSettings.style.top = '10px'
@@ -72,31 +82,37 @@ accPic.addEventListener('mouseleave', () => {
       accSettings.style.right = '80px'
     }, 100);
   }
-  })
+})
 
-  let SettingsPcValue = 0
-    accSettings.onclick = function() {
-      SettingsPcValue = 1
+let SettingsPcValue = 0
+accSettings.onclick = function () {
+  SettingsPcValue = 1
+}
+accContainer.addEventListener('click', () => {
+  setTimeout(() => {
+    if (window.innerWidth > 520 && SettingsPcValue == 0) {
+      accContainer.style.opacity = '0'
+      accPic.style.border = '0px solid #3ea6ff'
+      accSettings.style.right = '0px'
+      setTimeout(() => {
+        accContainer.style.display = 'none'
+      }, 300);
     }
-  accContainer.addEventListener('click', () => {
-    setTimeout(() => {
-      if(window.innerWidth > 520 && SettingsPcValue == 0) {
-    accContainer.style.opacity = '0'
-    accPic.style.border = '0px solid #3ea6ff'
-    accSettings.style.right = '0px'
-    setTimeout(() => {
-      accContainer.style.display = 'none'
-    }, 300);
-  }
-    }, 10);
+  }, 10);
   setTimeout(() => {
     SettingsPcValue = 0
   }, 20);
-  })
+})
 
-  accPic.addEventListener('click', () => {
-    if(window.innerWidth <= 520) {
-      accSettings.style.top = '100%'
+accPic.addEventListener('click', () => {
+  SettingsOpen()
+})
+closeSettings.addEventListener('click', () => {
+  SettingsClose()
+})
+function SettingsOpen() {
+  if (window.innerWidth <= 520) {
+    accSettings.style.top = '100%'
     accContainer.style.display = 'block'
     accPic.style.border = '1px solid #3ea6ff'
     setTimeout(() => {
@@ -104,9 +120,9 @@ accPic.addEventListener('mouseleave', () => {
       accSettings.style.top = '0'
     }, 10);
   }
-  })
-  closeSettings.addEventListener('click', () => {
-    if(window.innerWidth <= 520) {
+}
+function SettingsClose() {
+  if (window.innerWidth <= 520) {
     accContainer.style.opacity = '0'
     accPic.style.border = '0px solid #3ea6ff'
     accSettings.style.top = '100%'
@@ -114,12 +130,59 @@ accPic.addEventListener('mouseleave', () => {
       accContainer.style.display = 'none'
     }, 300);
   }
-  })
+}
+let OpenNotifications = document.querySelector('nav .third .notifications')
+let NotificationsSection = document.querySelector('.open-notifications ')
+let NotificationsValue = 0
+
+OpenNotifications.addEventListener('click', () => {
+  NotficitionClose()
+})
+
+function NotficitionClose() {
+  if (window.innerWidth > 680) {
+    if (NotificationsValue == 0) {
+      NotificationsSection.style.right = '125px'
+      NotificationsSection.style.display = 'flex'
+      NotificationsSection.style.opacity = '1'
+      NotificationsValue = 1
+      OpenNotifications.innerHTML = NotfiacationActive
+    } else if (NotificationsValue == 1 && window.innerWidth > 680) {
+      NotificationsSection.style.right = '-490px'
+      NotificationsSection.style.display = 'flex'
+      NotificationsSection.style.opacity = '1'
+      NotificationsValue = 0
+      OpenNotifications.innerHTML = Notfiacationidle
+    }
+  }
+  if (window.innerWidth < 680) {
+    if (NotificationsValue == 0) {
+      OpenNotifications.innerHTML = NotfiacationActive
+      NotificationsSection.style.display = 'flex'
+      setTimeout(() => {
+        NotificationsSection.style.bottom = '-54px'
+        NotificationsSection.style.opacity = '1'
+        NotificationsValue = 1
+      }, 10);
+    } else if (NotificationsValue == 1) {
+      OpenNotifications.innerHTML = Notfiacationidle
+      NotificationsSection.style.bottom = '-800px'
+      NotificationsSection.style.right = '0'
+      NotificationsValue = 0
+      setTimeout(() => {
+        NotificationsSection.style.display = 'none'
+        NotificationsSection.style.opacity = '0'
+        NotificationsSection.style.right = '0'
+      }, 200);
+    }
+  }
+}
 
 let MobileSearchContainer = document.querySelector('.m-search-container')
 let MobileSearchIcon = document.getElementById('Search')
 let MobileSearchBar = document.querySelector('.m-search')
 let MobileSearchBack = document.querySelector('.m-search .m-back')
+let MobileSearchInput = document.querySelector('.m-search .search-input input')
 
 let MobileSearchContainerValue = 0
 
@@ -127,10 +190,11 @@ MobileSearchBar.addEventListener('click', () => {
   MobileSearchContainerValue = 1
 })
 MobileSearchIcon.addEventListener('click', () => {
-  if(window.innerWidth <= 520) {
+  if (window.innerWidth <= 520) {
     MobileSearchContainer.style.display = 'flex'
     setTimeout(() => {
       MobileSearchBar.style.top = '0'
+      MobileSearchInput.focus()
     }, 10);
   }
 })
@@ -142,10 +206,10 @@ MobileSearchBack.addEventListener('click', () => {
 })
 MobileSearchContainer.addEventListener('click', () => {
   if (MobileSearchContainerValue == 0) {
-  MobileSearchBar.style.top = '-54px'
-  setTimeout(() => {
-    MobileSearchContainer.style.display = 'none'
-  }, 100);
+    MobileSearchBar.style.top = '-54px'
+    setTimeout(() => {
+      MobileSearchContainer.style.display = 'none'
+    }, 100);
   }
   MobileSearchContainerValue = 0
 })
@@ -156,24 +220,9 @@ let landing = document.querySelector('.landing')
 let optionClose = document.querySelector('.right-nav-open .Options')
 
 
-// function check() {
-//   if(window.innerWidth <= 520) {
-//     landing.style.transform = 'translateX(0px) translateY(54px)'
-//     landing.style.width = '100% '
-//     optionsValue = 1
-//     RightNav.style.left = '0px'
-//   } else if (window.innerWidth >= 520) {
-//     RightNav.style.left = '-72px'
-//   }
-// }
-
-// window.onresize = check
-
 optionOpen.addEventListener('click', () => {
-    RightNav.style.left = '0px'
+  RightNav.style.left = '0px'
 })
 optionClose.addEventListener('click', () => {
   RightNav.style.left = '-240px'
 })
-
-
